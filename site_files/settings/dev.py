e1877e8c5a1af583e988dd86ed123e3ceffd05c5
Rob Crowther <rw.crowther@gmail.com>
2020-10-06 19:54:23 +0100

    Settings

--- site_files/settings/dev.py
+++ site_files/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j7!+p#mt@!2^wx(b(u7g9!k836gfmk!w&m7^(o38j#o=7!fv^f'
+SECRET_KEY = #50 chars of alpanumericsymbols here!
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
